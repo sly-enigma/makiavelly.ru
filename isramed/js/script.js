5969828dce37035e6aee3ef64a2e3b5801de7164
Мы <5eumaelish@gmail.com>
2015-12-13 01:49:28 +0300

    добавлен слайдер

--- isramed/js/script.js
+++ isramed/js/script.js
@@ -1,1 +1,25 @@
+$(document).ready(function(){
+    $('.main-page-slider').owlCarousel({
+        loop:true,
+        navText: false,
+        autoWidth:true,
+        margin:25,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            600:{
+                items:3,
+                nav:false
+            },
+            1000:{
+                items:6,
+                nav:true,
+                loop:true
+            }
+        }
+    });
+});
 
